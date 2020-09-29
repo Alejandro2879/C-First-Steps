32b156dec0a977bbcbe098091589cb0edf8b2f15
Sergio <2016@holbertonschool.com>
2020-09-29 13:27:44 -0500

    Files: 0-reset..., 0-main.c and holberton.h added

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Modify the value of n in the main function.
+ *
+ * @n: is the variable that the main funtion send.
+ * Return: no return.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
