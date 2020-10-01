b3c1cfa9bc7721689d0ba7bc0074f7db8a09a127
Sergio <2016@holbertonschool.com>
2020-10-01 15:37:28 -0500

    File 2-strncpy.c added

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strncpy - Copy the string src on the dest array.
+ * @dest: destination array.
+ * @src: Source string.
+ * @n: Number of characters to copy.
+ * Return: Destination array.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int iter;
+
+	for (iter = 0; iter < n; iter++)
+	{
+		dest[iter] = src[iter];
+	}
+	return (dest);
+}
