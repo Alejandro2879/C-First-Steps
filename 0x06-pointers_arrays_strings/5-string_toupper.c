becfad05dce9e688f6aebe5a419dbc8dd66d050f
Sergio <2016@holbertonschool.com>
2020-10-01 20:09:40 -0500

    Files 5-string... and holberton.h added

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - Convert string in uppercase to lowercase
+ * @s: String in lowercase.
+ * Return: String in uppercase.
+ */
+char *string_toupper(char *s)
+{
+	char *str = s;
+
+	for (; *s; s++)
+		if (*s > 'a' && *s < 'z')
+			*s += -32;
+	return (str);
+}
