a5f07502bea624f9beb7f6d021655afa8698b38a
Sergio <2016@holbertonschool.com>
2020-10-10 00:49:40 -0500

    Files holberton.h 9-times_table.c added

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+
+/**
+ * times_table - function prints the 9 times table, starting with 0
+ *
+ * Return: No return.
+ */
+
+void times_table(void)
+{
+	int iter1, iter2, first_dig, second_dig, contain, numb;
+
+	for (iter1 = 0; iter1 <= 9; iter1++)
+	{
+		for (iter2 = 0; iter2 <= 9; iter2++)
+		{
+			numb = iter2 * iter1;
+			if (numb < 10)
+			{
+				_putchar('0' + numb);
+			}
+			else
+			{
+				first_dig = numb % 10;
+				contain = numb - first_dig;
+				second_dig = contain / 10;
+				_putchar('0' + second_dig);
+				_putchar('0' + first_dig);
+			}
+				if (iter2 < 9)
+				{
+					_putchar(44);
+					_putchar(32);
+					if (numb < 10)
+						_putchar(32);
+				}
+		}
+		_putchar('\n');
+	}
+}
