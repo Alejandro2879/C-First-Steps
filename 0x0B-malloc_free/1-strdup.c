e6c56928149b4ddc01b18a912348d0a04af66ffd
Sergio <2016@holbertonschool.com>
2020-10-13 12:14:26 -0500

    Files holberton.h 1-strdup.c 1-main.c added

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+char *_strdup(char *str)
+{
+	int iter;
+	char *new_str;
+
+	if (str == '\0')
+		return ('\0');
+
+	new_str = malloc(sizeof(str));
+	if (!new_str)
+		return ('\0');
+	for (iter = 0; str[iter]; iter++)
+	{
+		*(new_str + iter) = *(str + iter);
+	}
+	return (new_str);
+}
