0f7ae131a3c2d05ba34226641838486ed5a25f49
Sergio <2016@holbertonschool.com>
2020-10-13 13:05:05 -0500

    Add strlen function to malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,7 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * main - check the code for Holberton School students.
@@ -16,7 +17,7 @@ char *_strdup(char *str)
 	if (str == '\0')
 		return ('\0');
 
-	new_str = malloc(sizeof(str));
+	new_str = malloc(sizeof(str) * strlen(str));
 	if (!new_str)
 		return ('\0');
 	for (iter = 0; str[iter]; iter++)
