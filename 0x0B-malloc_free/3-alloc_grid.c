40b479c4f2c64560eab886449350c849efbca453
Sergio <2016@holbertonschool.com>
2020-10-13 16:09:40 -0500

    Files holberton.h 3-alloc_grid.c 3-main.c added

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Allocate memory for a bi-dimensional array.
+ * @width: width of the array
+ * @height: height of the array
+ * Return: Pointer to bidimensional array.
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **bi_dim, rows, iter, iter2;
+
+	if (width <= 0 || height <= 0)
+		return ('\0');
+	bi_dim = (int **) malloc(width * sizeof(int *));
+	if (!bi_dim)
+		return ('\0');
+
+	for (rows = 0; rows < width; rows++)
+	{
+		bi_dim[rows] = malloc(height * sizeof(int *));
+		if (!bi_dim[rows])
+			return ('\0');
+	}
+
+	for (iter = 0; iter < height; iter++)
+	{
+		for (iter2 = 0; iter2 < width; iter2++)
+		{
+			bi_dim[iter][iter2] = 0;
+		}
+	}
+	return (bi_dim);
+}
