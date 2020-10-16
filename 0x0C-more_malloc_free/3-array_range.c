fab5aac7b3515175644816243a05acc3eeed32a3
Sergio <2016@holbertonschool.com>
2020-10-16 13:28:58 -0500

    Files 3-main.c 3-array_range.c holberton.h added

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * array_range - Print number from min to max.
+ * @min: First number.
+ * @max: Last number.
+ * Return: Pointer to new array or NULL on fail.
+ */
+
+int *array_range(int min, int max)
+{
+	int *array, iter, size = (max - min) + 1;
+
+	if (min > max)
+		return ('\0');
+	array = malloc(size * sizeof(int));
+	if (!array)
+		return ('\0');
+
+	for (iter = 0; iter <= max; iter++)
+	{
+		array[iter] = min;
+			min += 1;
+	}
+	return (array);
+}
