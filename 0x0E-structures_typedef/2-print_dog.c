7966be3da4c42310d36d17aed63311945f588bcf
Sergio <2016@holbertonschool.com>
2020-10-20 13:11:26 -0500

    Fix For NULL values of name & owner

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,14 +14,14 @@ void print_dog(struct dog *d)
 		if (d->name)
 			printf("Name: %s\n", d->name);
 		else
-			printf("Name: (nil)\n");
+			printf("Name: %p\n", d->name);
 		if (d->age)
 			printf("Age: %f\n", d->age);
 		else
-			printf("Age: (nil)\n");
+			printf("Age: %f\n", d->age);
 		if (d->owner)
 			printf("Owner: %s\n", d->owner);
 		else
-			printf("Owner: (nil)\n");
+			printf("Owner: %p\n", d->owner);
 	}
 }
