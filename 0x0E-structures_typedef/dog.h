d385a810221cd960a3947b737af908642211d2bd
Sergio <2016@holbertonschool.com>
2020-10-20 11:03:43 -0500

    Create comments for the structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Describe a dog.
+ * @name: Name of the dog.
+ * @age: Age of the dog.
+ * @owner: Owner of the dog.
+ *
+ * Description: Describe a dog.
+ */
+
 struct dog
 {
 	char *name;
