131ce8c3694b1bbcf9d192abf04fc45f403b4892
Sergio <2016@holbertonschool.com>
2020-10-22 19:39:39 -0500

    Files 2-main.c 2-int_index.c function_pointers.h added

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * is_98 - check if a number is equal to 98
+ * @elem: the integer to check
+ *
+ * Return: 0 if false, something else otherwise.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int iter;
+
+	if (size <= 0)
+		return (-1);
+	for (iter = 0; iter < size; iter++)
+	{
+		if ((*cmp)(array[iter]))
+			return(iter);
+	}
+	return (-1);
+}
