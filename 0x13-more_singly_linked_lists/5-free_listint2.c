c10d97d77a62042067c1b80d90972e74e079d41f
Sergio <2016@holbertonschool.com>
2020-11-04 17:33:49 -0500

    Files 5-main.c 5-free_listint2.c lists.h added

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Free all nodes in a linked list.
+ * @head: Pointer to the head of the list.
+ * Return: No return.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *free_node;
+
+	if (*head)
+	{
+		while (*head)
+		{
+			free_node = *head;
+			*head = (*head)->next;
+			free(free_node);
+		}
+	}
+	head = NULL;
+}
