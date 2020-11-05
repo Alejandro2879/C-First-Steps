cfa034124a2095365ba50d73a72455c920e4f4b7
Sergio <2016@holbertonschool.com>
2020-11-04 20:04:37 -0500

    Files 6-pop_listint.c 6-main.c lists.h added

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node & returns the head node’s data (n).
+ * @head: Pointer to the head of the list.
+ * Return: Value of the lement n in the node.
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *free_node;
+	int element_n;
+
+	if (!head)
+		return (0);
+
+	free_node = *head;
+	*head = (*head)->next;
+	element_n = free_node->n;
+	free(free_node);
+	return (element_n);
+}
