4d89b7336ed5545f36259b829e5d0496a1602d0f
Sergio <2016@holbertonschool.com>
2020-11-04 21:19:31 -0500

    Correction to head = NULL

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int iter = 0;
 	listint_t *node = head;
 
-	if (!head)
+	if (!head || head == NULL)
 		return (NULL);
 
 	while (iter < index)
