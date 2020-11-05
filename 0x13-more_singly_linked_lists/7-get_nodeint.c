3ca1fc0f8dc91aa82527b3a86545cb8a87003007
Sergio <2016@holbertonschool.com>
2020-11-04 22:08:00 -0500

    Add check in case of the node does not exist

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -18,10 +18,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	while (iter < index)
 	{
 		node = node->next;
-		iter++;
+		if (node)
+			iter++;
+		else
+			return (NULL);
 	}
-	if (!node)
-		return (NULL);
-
 	return (node);
 }
