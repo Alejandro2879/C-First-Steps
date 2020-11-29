4120cb00212984214caf258dac4283a5cfc6bd67
Sergio <2016@holbertonschool.com>
2020-11-29 17:45:43 -0500

    Add function 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Finds the first 98 Fibonacci numbers, starting with 1 and 2
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int num1 = 1, num2 = 2, iter;
+	long int fibo = 0;
+
+	for (iter = 0; iter < 98; iter++)
+	{
+		fibo = num1 + num2;
+		num1 = num2;
+		num2 = fibo;
+		printf("%ld, ", fibo);
+	}
+	return (0);
+}
