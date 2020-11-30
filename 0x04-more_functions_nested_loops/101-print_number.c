1c3152a19c7c7fdb677980b40c1d4e22c01338df
Sergio <2016@holbertonschool.com>
2020-11-29 22:02:01 -0500

    Add function 101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+/**
+ * print_number - print number with _putchar function.
+ * @n: Number to print.
+ *
+ * Return: No return.
+ */
+
+void print_number(int n)
+{
+	int container = 0, num_a = 10, num_b;
+
+	if (n >= 0 && n < 10)
+		_putchar(n + 48);
+	else if (n < 0 && n > -10)
+	{
+		n = n * -1;
+		_putchar('-');
+		_putchar(n + 48);
+	}
+	else
+	{
+		if (n < 0)
+		{
+			n = n * -1;
+			_putchar('-');
+		}
+		container = n;
+		while (container / num_a > 9)
+		{
+			num_a = num_a * 10;
+		}
+		while (num_a > 0)
+		{
+			num_b = container / num_a;
+			container = container % num_a;
+			_putchar(num_b + 48);
+			num_a = num_a / 10;
+		}
+	}
+}
