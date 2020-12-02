a3e27b73d9f10adb0027f9e5d135ab03b98a51cf
Sergio <2016@holbertonschool.com>
2020-12-02 02:21:06 -0500

    Add file 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,11 +8,11 @@
 
 int main(void)
 {
-	int num1, num2, num3 = 0;
+	int num1 = 48, num2 = 49, num3 = 0;
 
-	for (num1 = 48; num1 <= 56; num1++)
+	for (; num1 <= 56; num1++)
 	{
-		for (num2 = 49; num2 <= 57; num2++)
+		for (; num2 <= 57; num2++)
 		{
 			putchar(num1);
 			putchar(num2);
@@ -23,7 +23,7 @@ int main(void)
 			}
 		}
 		num3++;
-		num3 = num3 + 49;
+		num2 = num3 + 49;
 	}
 	putchar('\n');
 	return (0);
