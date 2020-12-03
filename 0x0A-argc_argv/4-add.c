1d4af5ba0c4535b934f825835144d1aa541cb7e9
Sergio <2016@holbertonschool.com>
2020-12-03 14:35:19 -0500

    Add file 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,35 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-
+int _isdigit(char *s);
 /**
  * main - program that adds positive numbers.
  * @argc: Arguments count.
  * @argv: Arguments vector.
- * Return: 0 on success.
+ * Return: Always 0.
  */
 
 int main(int argc, char *argv[])
 {
-	int iter, contain, numb = 0;
+	int iter;
+	unsigned int result;
 
-	if (argc == 1)
+	if (argc < 3)
 	{
 		printf("0\n");
+		return (0);
 	}
+
 	for (iter = 1; iter < argc; iter++)
 	{
-		if (isdigit(argv[iter]) != 0)
-		{
-			contain = atoi(argv[iter]);
-			numb += contain;
-		}
+		if (_isdigit(argv[iter]))
+			result += atoi(argv[iter]);
 		else
 		{
-			printf("Error");
+			printf("Error\n");
 			return (1);
 		}
 	}
-	printf("%d\n", numb);
+	printf("%d\n", result);
 	return (0);
+}
+
+/**
+ * _isdigit - Check if there is a digit.
+ * @s: String to check.
+ * Return: 1 on success, 0 on fail.
+ */
+
+int _isdigit(char *s)
+{
+	int iter;
+
+	for (iter = 0; s[iter]; iter++)
+	{
+		if (!isdigit(s[iter]))
+			return (0);
+	}
+	return (1);
 }
