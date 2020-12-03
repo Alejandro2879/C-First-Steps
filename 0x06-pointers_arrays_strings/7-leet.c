0bb488776afc6b995678f187b76a157e0fd4f38b
Sergio <2016@holbertonschool.com>
2020-12-03 16:47:52 -0500

    Add file 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,13 +2,32 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * leet - that encodes a string into 1337.
+ * @s: String to perform.
+ * Return: New string.
  */
 
 char *leet(char *s)
 {
-	int iter = 0;
+	int i = 0;
+	int j;
+	char let1[] = "AEOTL";
+	char numbers[] = "43071";
+	char let2[] = "aeotl";
+
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (numbers[j] != '\0')
+		{
+			if (str[i] == let1[j] || str[i] == let2[j])
+			{
+				str[i] = numbers[j];
+			}
+			j++;
+		}
+		i++;
+	}
+	return (str);
 
 }
