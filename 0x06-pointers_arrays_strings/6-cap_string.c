4048d8a1baf10e75f39a7e35edf40d153ca26e80
Sergio <2016@holbertonschool.com>
2020-12-03 16:28:47 -0500

    Add files 6-cap_string.c holberton.h

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,27 +2,32 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
+ * cap_string - capitalizes all words of a string..
+ * @tx: String
  * Return: Always 0.
  */
 char *cap_string(char *tx)
 {
-	int c1;
+	int c1, c2 = 1;
 
-	for (c1 = 0; tx[iter]; c1++)
+	for (c1 = 0; tx[c1] != '\0'; c1++)
 	{
+		if (tx[c1] >= 'A' &&  tx[c1] <= 'Z')
+			c2 = 0;
+		else if (tx[c1] >= '0' &&  tx[c1] <= '9')
+			c2 = 0;
+		else if
+			((tx[c1] >= 'a' && tx[c1] <= 'z') && c2 == 1)
+			{
+			     tx[c1] -= 32;
+			     c2 = 0;
+			}
 		if (tx[c1] == 46 || tx[c1] == 59 || tx[c1] == 44
 		    || tx[c1] == 33 || tx[c1] == 63 || tx[c1] == 34
 		    || tx[c1] == 40 || tx[c1] == 41 || tx[c1] == 123
 		    || tx[c1] == 125 || tx[c1] == 32 || tx[c1] == 9
 		    || tx[c1] == 10)
-		{}
-		if (tx[iter + 1] == 32)
-		{
-			tx[iter + 2] -= 32;
-		}
-
-	
+			c2 = 1;
 	}
+	return (tx);
 }
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -9,5 +9,6 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 void print_number(int n);
+char *cap_string(char *);
 
 #endif /*HOLBERTON_H*/
