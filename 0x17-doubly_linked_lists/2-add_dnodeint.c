d8fff06112dfdf57a9d2873dc907bab79c48ed5f
Sergio <2016@holbertonschool.com>
2020-12-14 21:39:59 -0500

    Add files 2-main.c 2-add_dnodeint.c lists.h

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint - Add a node at the beginning of a linked list.
+ * @head: Pointer to the head.
+ * @n: Date for n.
+ * Return: Pointer to the new node.
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = NULL;
+
+	new_node = malloc(sizeof(new_node));
+	if (!new_node)
+		return (NULL);
+	if (*head == NULL)
+	{
+		*head = new_node;
+		new_node->prev = NULL;
+		new_node->next = NULL;
+	}
+	else
+	{
+		new_node->n = n;
+		(*head)->prev = new_node;
+		new_node->next = *head;
+		new_node->prev = NULL;
+		*head = new_node;
+	}
+	return (new_node);
+}
