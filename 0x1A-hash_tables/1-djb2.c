db1648525f835a7ac892f888a81672cdefce369a
Sergio <2016@holbertonschool.com>
2021-01-15 23:13:35 -0500

    Add files 1-main.c 1-djb2.c

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+/**
+ * hash_djb2 - Impliment the djb2 algorithm
+ * @str: String to compute.
+ * Return: Index
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	if (str == NULL)
+		return ("\0");
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return ((unsigned long int)hash);
+}
