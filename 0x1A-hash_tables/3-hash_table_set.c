359066bb5a8ee3bacea325e4cce86e84438c97b7
Sergio <2016@holbertonschool.com>
2021-01-17 10:57:03 -0500

    Add file 3-hash_table.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -28,8 +28,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	node->key = strdup(key);
 	node->value = strdup(value);
-
-
+	if (!(node->key) || !(node->value))
+	{
+		if (node->key)
+			free(node->key);
+		free(node);
+		return (0);
+	}
 	node->next = ht->array[index];
 	ht->array[index] = node;
 	return (1);
